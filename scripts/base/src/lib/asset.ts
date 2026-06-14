7ecef89a53fe7dc5c26bc4dbc5eb7e8dbb0442f7
Nida <nida@Nidas-MacBook-Pro.local>
2026-06-14 16:09:40 +0700

    chore: remove logger

--- scripts/base/src/lib/asset.ts
+++ scripts/base/src/lib/asset.ts
@@ -1,4 +1,3 @@
 const CDN_BASE = import.meta.env.PROD ? import.meta.env.VITE_CDN_BASE_URL : '';
 
-console.log('CDN_BASE:', CDN_BASE);
 export const assetUrl = (path: string): string => `${CDN_BASE}${path}`;
